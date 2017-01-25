052140ad14b477185db065ab2f4e1cb8ab2d2cf3
Min Soo Kim <minskim92@gmail.com>
2017-01-25 14:11:22 -0500

    added PATCH /todos/:id

--- server.js
+++ server.js
@@ -1,5 +1,6 @@
 const express = require('express'),
       bodyParser = require('body-parser'),
+      _ = require('lodash'),
       { ObjectID } = require('mongodb');
 
 const { mongoose } = require('./db/mongoose'),
@@ -65,6 +66,31 @@ app.delete('/todos/:id', (req, res) => {
   }).catch(err => {
     res.status(400).send();
   });
+
+});
+
+app.patch('/todos/:id', (req, res) => {
+  let id = req.params.id;
+  let body = _.pick(req.body, ['text', 'completed']);
+
+  if (!ObjectID.isValid(id)) {
+    return res.status(404).send();
+  }
+
+  if (_.isBoolean(body.completed) && body.completed) {
+    body.completedAt = new Date().getTime();
+  } else {
+    body.completed = false;
+    body.completedAt = null;
+  }
+
+  Todo.findByIdAndUpdate(id, {$set: body}, {new: true}).then(todo => {
+    if (!todo) {
+      return res.status(404).send();
+    }
+
+    res.send({todo});
+  }).catch(e => res.status(400).send());
 
 });
 
