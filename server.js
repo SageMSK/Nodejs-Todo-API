8cff34cfa4dd24a16c2b3542d175949ddf5a2484
Min Soo Kim <minskim92@gmail.com>
2017-01-25 10:24:40 -0500

    added test for DELETE /todos/:id

--- server.js
+++ server.js
@@ -49,6 +49,25 @@ app.get('/todos/:id', (req, res) => {
   });
 });
 
+app.delete('/todos/:id', (req, res) => {
+  let id = req.params.id;
+
+  if(!ObjectID.isValid(id)) {
+    return res.status(404).send();
+  }
+
+  Todo.findByIdAndRemove(id).then(todo => {
+    if (!todo) {
+      return res.status(404).send();
+    }
+
+    res.send({todo});
+  }).catch(err => {
+    res.status(400).send();
+  });
+
+});
+
 app.listen(PORT, () => {
   console.log(`Listening on port ${PORT}...`);
 });
